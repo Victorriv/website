36292becec3bbbe40e29918941ecc6c76209d4f3
Victorriv <Victorrivera714@yahoo.com>
2024-10-13 12:50:18 -0700

    some basics learned

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-console.log(`hello`);
+/*console.log(`hello`);
 console.log(`I like pizza`);
 
 
 
-window.alert(`This is an alert`); // this gives alert to the server. 
+window.alert(`This is an alert`);
+ */
+
+
+// this gives alert to the server. 
+
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = "I like pizzas"; // to change content of html element you can to select the element "get elemente by" then set it equal to text you want . 
+
+// basic output you can use console.log 
+// to create alert you just use alert
